42ac16e231512ce3f2d93e5dd0aacfa0d3143543
tangyating <tangyating@dxy.cn>
2022-09-29 14:54:41 +0800

    fix -l 改良版计数排序(新增一个min)之前版本不可排序负数

--- count-sort.js
+++ count-sort.js
@@ -1,17 +1,40 @@
-const CountSort =  (array)=>{
+// const CountSort =  (array)=>{
+//     let max = -Infinity
+//     for(let v of array){
+//         max = Math.max(v,max)
+//     }
+//     const count = new Array(max+1).fill(0)
+//     for(let v of array){
+//         count[v]++
+//     }
+//     const res=[]
+//     for(let i = 0;i<count.length;i++){
+//         let temp = count[i]
+//         while(temp--){
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
+
+const countStart = (arr) => {
     let max = -Infinity
-    for(let v of array){
-        max = Math.max(v,max)
+    let min = Infinity
+    for(let v of arr){
+        max = Math.max(max,v)
+        min = Math.min(min,v)
     }
-    const count = new Array(max+1).fill(0)
-    for(let v of array){
-        count[v]++
+    const len = (max - min) + 1
+    const array = new Array(len).fill(0)
+
+    for(let v of arr){
+        array[v-min] ++
     }
-    const res=[]
-    for(let i = 0;i<count.length;i++){
-        let temp = count[i]
+    let res = []
+    for(let i in array){
+        let temp = array[i]
         while(temp--){
-            res.push(i)
+            res.push(parseInt(i)+min)
         }
     }
     return res
